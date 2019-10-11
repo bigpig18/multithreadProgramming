955fe3a54f3a5e061396d61db7317ff86115b0f5
liyun <2726829719@qq.com>
2019-10-11 11:55:32 +0800

    "线程基础 - ExceptionInChildThread - 添加注释 "

--- thread-core/src/createthread/uncaughtexception/ExceptionInChildThread.java
+++ thread-core/src/createthread/uncaughtexception/ExceptionInChildThread.java
@@ -17,6 +17,10 @@ public class ExceptionInChildThread implements Runnable{
 
     @Override
     public void run() {
+        //子线程抛出异常，主线程未受影响
+        //子线程抛出的异常日志很有可能被覆盖在海量日志中
+        //所以说  主线程可以轻松发现异常，但是子线程不行
+        //同时  子线程异常我们无法用传统的方法捕获
         throw new RuntimeException();
     }
 }
