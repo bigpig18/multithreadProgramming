955be3f0f6b1a8d84f0d7d27126310ee472289c4
liyun <2726829719@qq.com>
2019-10-14 16:05:06 +0800

    "线程基础 - 线程安全 - RegisterListenerThreadError - 添加一些必要注释"

--- thread-core/src/createthread/threaderrors/RegisterListenerThreadError.java
+++ thread-core/src/createthread/threaderrors/RegisterListenerThreadError.java
@@ -2,7 +2,7 @@ package createthread.threaderrors;
 
 /**
  * 描述: 注册监听器(观察者模式)
- *
+ * 还未初始化(构造函数没完全执行完毕)就把对象提供给了外界
  * @author li
  * @date 2019/10/12
  */
@@ -29,6 +29,8 @@ public class RegisterListenerThreadError {
             }
             mySource.eventCome(new Event(){});
         }).start();
+        //这里使用构造方法 其中count 应该在构造方法中赋值了 是100 但是打印结果可能不一样
+        //在这里 还未初始化(构造函数没完全执行完毕)就把对象提供给了外界
         RegisterListenerThreadError error = new RegisterListenerThreadError(mySource);
     }
 
