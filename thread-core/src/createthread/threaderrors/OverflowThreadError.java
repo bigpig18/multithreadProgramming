8e00ba14f2526db78eb44fd04c5faff594ba5800
liyun <2726829719@qq.com>
2019-10-14 15:54:29 +0800

    "线程基础 - 线程安全 - OverflowThreadError - 用副本方式解决问题"

--- thread-core/src/createthread/threaderrors/OverflowThreadError.java
+++ thread-core/src/createthread/threaderrors/OverflowThreadError.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class OverflowThreadError {
 
     /**
-     *  private 本意是不让外部应用访问到
+     *  private 本意是不让外部访问到
      */
     private Map<String,String> states;
 
@@ -30,12 +30,27 @@ public class OverflowThreadError {
         return states;
     }
 
+    /**
+     * 用副本的方法解决该线程安全问题
+     * @return map
+     */
+    public Map<String,String> getStatesImproved(){
+        //这样就是只传一个副本出去  外部不能修改 private 修饰的 states
+        return new HashMap<>(states);
+    }
+
     public static void main(String[] args) {
         OverflowThreadError error = new OverflowThreadError();
         Map<String,String> map = error.getStates();
         System.out.println(map.get("1"));
         //在这里  我们可以修改private 修饰的变量
         map.remove("1");
-        System.out.println(map.get("1"));
+        System.out.println(error.getStates().get("1"));
+
+        map = error.getStatesImproved();
+        System.out.println(map.get("2"));
+        //用了副本的方法 看是否还会被修改private修饰的变量
+        map.remove("2");
+        System.out.println(error.getStatesImproved().get("2"));
     }
 }
