fa377a7a887c45248197c067acaa980febe4ebed
liyun <2726829719@qq.com>
2019-10-18 11:24:18 +0800

    "线程基础 - Java内存模型 - 应用方面(单例模式) - 饿汉式(静态代码块) 可用"

---
+++ thread-core/src/singleton/SingletonHungry2.java
@@ -0,0 +1,20 @@
+package singleton;
+
+/**
+ * 描述: 饿汉式(静态代码块) - 可用
+ * 这种写法跟第一种十分类似
+ * @author li
+ * @date 2019/10/18
+ */
+public class SingletonHungry2 {
+    private final static SingletonHungry2 INSTANCE;
+    static {
+        INSTANCE = new SingletonHungry2();
+    }
+
+    private SingletonHungry2(){};
+
+    public SingletonHungry2 getInstance(){
+        return INSTANCE;
+    }
+}
