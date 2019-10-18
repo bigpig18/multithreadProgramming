32db5f8afc1dfc9602ed730f522ec54acfc684fc
liyun <2726829719@qq.com>
2019-10-18 16:58:12 +0800

    "线程基础 - Java内存模型 - 应用方面(单例模式) - 双重检查锁模式 - 升级并添加必要注释"

--- thread-core/src/singleton/SingletonDuplicatingCheck.java
+++ thread-core/src/singleton/SingletonDuplicatingCheck.java
@@ -11,7 +11,16 @@ package singleton;
  */
 public class SingletonDuplicatingCheck {
 
-    private static SingletonDuplicatingCheck instance;
+    /**
+     * 这里加上volatile关键字是为了防止NPE，即空指针错误
+     * 这是因为 创建对象的时候有三个步骤:
+     * 1.为对象分配空间 2.调用构造函数 3.让对象引用指向该对象(这个时候instance就不为null了)
+     * 可是 1 2 3 步骤在 jvm或者cpu的重排序下有可能变成 1 3 2
+     * 这个时候 线程1 进入instance = new SingletonDuplicatingCheck() 语句，并执行了 1 3 步骤 ，还未给对象用构造函数创建对象里面的东西
+     * 然后突然切换到 线程2 线程2 在第一重判断 instance == null 这里，因为线程1 执行了3 步骤，instance不为空了 直接就返回给线程2 一个
+     * 还未经过 2 步骤的实例，这是用去操作实例，就会产生 NPE 错误了
+     */
+    private volatile static SingletonDuplicatingCheck instance;
 
     private SingletonDuplicatingCheck(){}
 
