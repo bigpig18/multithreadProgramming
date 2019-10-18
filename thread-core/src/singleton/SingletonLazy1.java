444f7216eaa90e757dfa0b65518cafd1946fc44e
liyun <2726829719@qq.com>
2019-10-18 11:37:04 +0800

    "线程基础 - Java内存模型 - 应用方面(单例模式) - 懒汉式(线程不安全) 不可用"

--- thread-core/src/singleton/SingletonLazy1.java
+++ thread-core/src/singleton/SingletonLazy1.java
@@ -16,7 +16,7 @@ public class SingletonLazy1 {
      * 这个方法，我们考虑两个线程竞争的情况
      * @return instance
      */
-    public SingletonLazy1 getInstance(){
+    public static SingletonLazy1 getInstance(){
         //两个线程同时到达这个判断语句，都判断instance为空了，然后都执行下班的语句
         if (instance == null){
             //而后两个线程都创建了instance实例 这个时候就不符合单例的需求了
